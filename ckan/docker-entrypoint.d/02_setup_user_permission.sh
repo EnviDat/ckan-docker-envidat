2386c6090614f6976497a8da4905662e6f6e4638
palranit <ranita.pal@wsl.ch>
2025-10-02 18:14:32 +0200

    Changes with fixing user permissions

--- ckan/docker-entrypoint.d/02_setup_user_permission.sh
+++ ckan/docker-entrypoint.d/02_setup_user_permission.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-echo '*** Setup User permission in /srv/app directory'
-chown -R ckan-sys:ckan-sys .
-
 echo '*** Setup User create ckan/i18n directory'
 mkdir -p /var/lib/ckan/i18n
 
 echo '*** Setup User permission in volume directory (/var/lib/ckan)'
-chown -R ckan-sys:ckan-sys /var/lib/ckan
-
-# change user to ckan-sys during setup "entrypoint" runtime
-# exec su ckan-sys -s /bin/bash -c "$*"
+chown -R ckan:ckan-sys /var/lib/ckan
