8ff6b576799dc1db6982a9b2501893893ab61d8b
palranit <ranita.pal@wsl.ch>
2025-10-06 16:30:28 +0200

    commenting out echo statements

--- ckan/docker-entrypoint.d/02_setup_user_permission.sh
+++ ckan/docker-entrypoint.d/02_setup_user_permission.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo '*** Setup User create ckan/i18n directory'
+#echo '*** Setup User create ckan/i18n directory'
 #mkdir -p /var/lib/ckan/i18n
 
-echo '*** Setup User permission in volume directory (/var/lib/ckan)'
+#echo '*** Setup User permission in volume directory (/var/lib/ckan)'
 #chown -R ckan:ckan-sys /var/lib/ckan
